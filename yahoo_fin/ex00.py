f4019f70dd834cf3eee911e1da8b4e86bab33f61
Michael Angerman <stormasm@gmail.com>
2020-05-06 11:05:38 -0700

    ex00

--- yahoo_fin/ex00.py
+++ yahoo_fin/ex00.py
@@ -1,3 +1,7 @@
+
+#print(type(ui))
+#<class 'pandas.core.frame.DataFrame'>
+
 from yahoo_fin.stock_info import get_data, tickers_sp500, tickers_nasdaq, tickers_other, get_quote_table
 
 ui = get_data("UI")

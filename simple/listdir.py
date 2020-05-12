e60fcc7c01ad7ed7a88f519b4043106675a64d2d
Michael Angerman <stormasm@gmail.com>
2020-05-12 16:39:55 -0700

    reading all files in the directory

--- simple/listdir.py
+++ simple/listdir.py
@@ -1,10 +1,11 @@
+
 import os
+import pandas as pd
+
 your_path = '/j/tmp32/equity-data/sp500/groups/200507'
 files = os.listdir(your_path)
 for file in files:
-    print(file)
-    if os.path.isfile(file):
-        f=open(os.path.join(your_path,file),'r')
-        for x in f:
-            print(x)
-        f.close()
+    filename = os.path.join(your_path,file)
+    print(filename)
+    df = pd.read_csv(filename, sep=',')
+    print(df)

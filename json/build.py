3ddadce7e0817b9e3d0cc8f1fec765c08fa85123
Michael Angerman <stormasm@gmail.com>
2020-05-13 16:47:54 -0700

    mw

--- json/build.py
+++ json/build.py
@@ -2,18 +2,14 @@
 import os
 import pandas as pd
 
-
-def get_series_array(series):
-    print(series.values)
-
 def process(filename):
     df = pd.read_csv(filename, sep=',')
-    symbols_series = df['Symbol']
-    get_series_array(symbols_series)
+    series = df['Symbol']
+    values = series.values
+    print(filename,values)
 
 your_path = '/j/tmp32/python-examples/data/sp500'
 files = os.listdir(your_path)
 for file in files:
     filename = os.path.join(your_path,file)
-    print(filename)
     process(filename)

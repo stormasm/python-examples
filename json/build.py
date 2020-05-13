b1ed7730f3b43556fc2996f842cef0cc654e1aa2
Michael Angerman <stormasm@gmail.com>
2020-05-12 18:55:20 -0700

    mw

--- json/build.py
+++ json/build.py
@@ -2,10 +2,13 @@
 import os
 import pandas as pd
 
+def process(filename):
+    df = pd.read_csv(filename, sep=',')
+    print(df)
+
 your_path = '/j/tmp32/python-examples/data/sp500'
 files = os.listdir(your_path)
 for file in files:
     filename = os.path.join(your_path,file)
     print(filename)
-    df = pd.read_csv(filename, sep=',')
-    print(df)
+    process(filename)

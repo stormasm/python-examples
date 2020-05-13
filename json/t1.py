a3cadaabdff643e081a526c76683de397ba320f6
Michael Angerman <stormasm@gmail.com>
2020-05-13 10:10:58 -0700

    t1.py

---
+++ json/t1.py
@@ -0,0 +1,15 @@
+import json
+'''
+t1 = json.dumps(['stu', {'bar': ('baz', None, 1.0, 2)},
+                'rick', {'bar': ('baz', None, 1.0, 2)},
+                'pete', {'bar': ('baz', None, 1.0, 2)},
+                ])
+
+print(t1)
+'''
+t2 = json.dumps([{"b": ("apd","avy","bll")},
+                 {"c": ("atvi","chtr","cmcsa")},
+                 {"e": ("apa","cog","cop")},
+                ])
+
+print(t2)

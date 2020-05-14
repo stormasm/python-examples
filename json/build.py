6eeaeab5c4ecd140618b02cbd0e1a16bf1172f7e
Michael Angerman <stormasm@gmail.com>
2020-05-13 17:38:55 -0700

    mw

--- json/build.py
+++ json/build.py
@@ -1,13 +1,18 @@
 import os
 import pandas as pd
+from pathlib import PurePath
 
+def get_symbol_from_filename(filename):
+    pp = PurePath(filename)
+    p1 = pp.parts
+    print(p1[-1])
 
 def process(filename):
     df = pd.read_csv(filename, sep=',')
     series = df['Symbol']
     values = series.values
-    print(filename, values)
-
+    get_symbol_from_filename(filename)
+    #print(filename, values)
 
 your_path = '/j/tmp32/python-examples/data/sp500'
 files = os.listdir(your_path)

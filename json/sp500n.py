8e23a245eaec7962c87f79c5e794054d4aff4f57
Michael Angerman <stormasm@gmail.com>
2020-05-14 11:02:37 -0700

    symbol - company name is up and running

--- json/sp500n.py
+++ json/sp500n.py
@@ -1,20 +1,9 @@
 import json
 import os
 import pandas as pd
-from pathlib import PurePath
 import re
 
-def get_symbol_from_filename(filename):
-    pp = PurePath(filename)
-    # get the filename in the path
-    p1 = pp.parts[-1]
-    # get everything before the dot
-    p2 = re.split("\.",p1)[0]
-    # get everything after holdings-
-    p3 = re.split("holdings-",p2)[1]
-    return(p3)
-
-def get_dict(filename):
+def get_tuples(filename):
     df = pd.read_csv(filename, sep=',')
     sseries = df['Symbol']
     svalues = sseries.values
@@ -34,8 +23,10 @@ files = os.listdir(your_path)
 d = {}
 for file in files:
     filename = os.path.join(your_path, file)
-    tsn = get_dict(filename)
-    print(tsn[0])
-    print(tsn[1])
-#myj = json.dumps(arr)
-#print(myj)
+    tsn = get_tuples(filename)
+    symbols = tsn[0]
+    names = tsn[1]
+    for s, n in zip(symbols, names):
+        d[s] = n
+myj = json.dumps(d)
+print(myj)

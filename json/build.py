f58678ec46c9a8e9292d0f473694b65a0a08bc9e
Michael Angerman <stormasm@gmail.com>
2020-05-13 18:06:16 -0700

    mw

--- json/build.py
+++ json/build.py
@@ -6,15 +6,18 @@ import re
 def get_symbol_from_filename(filename):
     pp = PurePath(filename)
     p1 = pp.parts[-1]
-    p2 = re.split("\.",p1)
-    print(p2)
+    # get everything before the dot
+    p2 = re.split("\.",p1)[0]
+    # get everything after holdings-
+    p3 = re.split("holdings-",p2)[1]
+    return(p3)
 
 def process(filename):
     df = pd.read_csv(filename, sep=',')
     series = df['Symbol']
     values = series.values
-    get_symbol_from_filename(filename)
-    #print(filename, values)
+    symbol = get_symbol_from_filename(filename)
+    print(symbol)
 
 your_path = '/j/tmp32/python-examples/data/sp500'
 files = os.listdir(your_path)

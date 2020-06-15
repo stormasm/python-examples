1d0b31beee851c30213bd3936353e9a53d60d3cf
Michael Angerman <stormasm@gmail.com>
2020-06-15 12:52:21 -0700

    ex03

--- yahoo_fin/ex03.py
+++ yahoo_fin/ex03.py
@@ -1,12 +1,17 @@
-
+import datetime
 
 # This is a Python set which is nice because we can not
 # get any duplication of data...
 top = {"ui","psa","ip","t"}
 top.add("nly")
 
+def get_day():
+    now = datetime.datetime.now()
+    print(now)
+
 def build_file_names(top):
     for val in top:
         print(val)
 
-build_file_names(top)
+#build_file_names(top)
+get_day()

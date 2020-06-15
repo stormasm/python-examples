92f16f3219725ea8a051011de5682e840880505f
Michael Angerman <stormasm@gmail.com>
2020-06-15 13:32:51 -0700

    ex03 is up and running and writing out the fundamental data into the appropriate file names

--- yahoo_fin/ex03.py
+++ yahoo_fin/ex03.py
@@ -1,20 +1,26 @@
 import datetime
+from yahoo_fin.stock_info import get_stats
 
 # This is a Python set which is nice because we can not
 # get any duplication of data...
-top = {"ui","psa","ip","t"}
-top.add("nly")
+symbols = {"ui","psa","ip","t"}
+symbols.add("nly")
 
 def get_day():
     x = datetime.datetime.now()
     y = x.strftime("%y-%m-%d")
     return y
 
-def build_file_names(top):
-    for val in top:
-        day = get_day()
-        x = f"{val}-fun-{day}"
-        print(x)
+def build_file_name(symbol):
+    day = get_day()
+    filename = f"{symbol}-fun-{day}.csv"
+    return(filename)
 
-build_file_names(top)
-#get_day()
+def process():
+    for symbol in symbols:
+        print(symbol)
+        filename = build_file_name(symbol)
+        data = get_stats(symbol)
+        data.to_csv(filename)
+
+process()

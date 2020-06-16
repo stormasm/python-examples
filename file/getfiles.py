691fd64b2d6f59e03dec3f51a482d8f916c6b85d
Michael Angerman <stormasm@gmail.com>
2020-06-16 10:37:36 -0700

    add check to see if it is being called by a __main__ program

--- file/getfiles.py
+++ file/getfiles.py
@@ -5,6 +5,7 @@ def getfiles(mypath):
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
     return(onlyfiles)
 
-path1 = '/j/tmp32/bluemesa/tmp'
-myfiles = getfiles(path1)
-print(myfiles)
+if __name__ == "__main__":
+    path1 = '/j/tmp32/bluemesa/tmp'
+    myfiles = getfiles(path1)
+    print(myfiles)

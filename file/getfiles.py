45372b4c24b48d05ac46265beb9537024656ab2a
Michael Angerman <stormasm@gmail.com>
2020-06-16 10:05:43 -0700

    getfiles

--- file/getfiles.py
+++ file/getfiles.py
@@ -1,8 +1,10 @@
-
 from os import listdir
 from os.path import isfile, join
 
-mypath = '/j/tmp32/bluemesa/tmp'
+def getfiles(mypath):
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    return(onlyfiles)
 
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-print(onlyfiles)
+path1 = '/j/tmp32/bluemesa/tmp'
+myfiles = getfiles(path1)
+print(myfiles)

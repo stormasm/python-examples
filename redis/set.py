167e7edde52b4d37582181eb271fb6659f6421b7
Michael Angerman <stormasm@gmail.com>
2020-06-20 14:06:07 -0700

    redis set is up and running

---
+++ redis/set.py
@@ -0,0 +1,19 @@
+import redis
+
+def redis_set_to_set(key):
+    members = set()
+    rset = r.smembers(key)
+    for value in rset:
+        value = value.decode("utf-8")
+        members.add(value)
+    return(members)
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+r.sadd('testset','red')
+r.sadd('testset','yellow')
+r.sadd('testset','green')
+members = r.smembers('testset')
+print(type(members))
+print(members)
+myset = redis_set_to_set('testset')
+print(myset)

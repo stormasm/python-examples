687be6582a2d79998fb4ef6b9b2b55c440973ed0
Michael Angerman <stormasm@gmail.com>
2020-06-28 11:19:14 -0700

    writefun is now taking in a 2 dimensional array

--- csv/writefun.py
+++ csv/writefun.py
@@ -7,10 +7,17 @@ def read_test():
             print(', '.join(row))
 
 def write_csv1(index,symbol,dyield,payout):
-    with open('eggs.csv', 'w', newline='') as csvfile:
+    with open('eggs1.csv', 'w', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',')
         spamwriter.writerow(['index'] + ['symbol'] + ['yield'] + ['payout'])
         spamwriter.writerow([index,symbol,dyield,payout])
+
+def write_csv2(data):
+    with open('eggs2.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',')
+        spamwriter.writerow(['index'] + ['symbol'] + ['yield'] + ['payout'])
+        for row in data:
+            spamwriter.writerow([row[0]] + [row[1]] + [row[2]] + [row[3]])
 
 def write_test_1():
     c1 = "1"
@@ -19,5 +26,15 @@ def write_test_1():
     c4 = "10%"
     write_csv1(c1,c2,c3,c4)
 
+def write_test_2():
+    arr = []
+    a1 = ['1','2','3','4']
+    arr.append(a1)
+    a2 = ['11','12','13','14']
+    arr.append(a2)
+    a3 = ['21','22','23','24']
+    arr.append(a3)
+    write_csv2(arr)
+
 if __name__ == "__main__":
-    write_test_1()
+    write_test_2()

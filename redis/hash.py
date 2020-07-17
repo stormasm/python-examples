7a3224531b22bdcb4ad698531c0692dc5c75d723
Michael Angerman <stormasm@gmail.com>
2020-07-17 09:20:36 -0700

    redis_hash_to_python_dict up and running

--- redis/hash.py
+++ redis/hash.py
@@ -11,18 +11,21 @@ def get_symbol_name(symbol):
     return(name)
 
 def redis_hash_to_python_dict(key):
-    members = set()
-    rset = r.smembers(key)
-    for value in rset:
-        value = value.decode("utf-8")
-        members.add(value)
-    return(members)
+    mydict = {}
+    symbols = rc.hkeys(key)
+    for symbol in symbols:
+        symbol = symbol.decode("utf-8")
+        name = get_symbol_name(symbol)
+        mydict[symbol] = name
+    return(mydict)
 
 write_symbol_to_hash('testhash','aapl','apple computer')
 write_symbol_to_hash('testhash','ui','ubiquiti networks')
 write_symbol_to_hash('testhash','amzn','amazon')
 
-result = rc.hgetall('testhash')
-print(result)
+hash = rc.hgetall('testhash')
+print(type(hash))
+print(hash)
 
 print(get_symbol_name('ui'))
+print(redis_hash_to_python_dict('testhash'))

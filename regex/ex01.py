039c94ab39cff04c1214de3496b4cec7f2a426fb
Michael Angerman <stormasm@gmail.com>
2020-07-23 14:53:41 -0700

    mw

---
+++ regex/ex01.py
@@ -0,0 +1,11 @@
+import re
+
+def remove_unwanted_chars(input):
+    c = re.sub('[M,B,%]', '', input)
+    return(c)
+
+# Replace every M,B,% character with nothing:
+a = ["123.45B","23B","234M","434.12B","34%","56.1%"]
+for x in a:
+    value = remove_unwanted_chars(x)
+    print(value)

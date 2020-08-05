d8ebe26a7e432ad6bc6525081bf20cee50ca3962
Michael Angerman <stormasm@gmail.com>
2020-08-04 17:01:21 -0700

    regex date parse is working

--- regex/ex02.py
+++ regex/ex02.py
@@ -1,10 +1,17 @@
 import re
 
 def get_date_from_filename(input):
-    c = re.search('(-)[0-9]*(_)', input)
-    return(c)
+    p = re.compile('(-)[0-9]*(_)')
+    q = p.search(input)
+    x = q.group()
+    x = x[1:9]
+    return(x)
 
-a = ["sq-20200331_htm.xml","cube-20200331_htm.xml"]
+a = ["sq-20191231_htm.xml","cube-20200331_htm.xml"]
 for x in a:
     value = get_date_from_filename(x)
     print(value)
+
+# references
+# https://docs.python.org/3/howto/regex.html
+# https://www.pythoncentral.io/how-to-get-a-substring-from-a-string-in-python-slicing-strings/

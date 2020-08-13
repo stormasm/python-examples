93bdb61c665ab19e96c64bc31a6672e6ebee091c
Michael Angerman <stormasm@gmail.com>
2020-08-13 09:24:24 -0700

    initial commit of date checking files

---
+++ date/validate.py
@@ -0,0 +1,9 @@
+
+class ValidDate(object):
+
+### Look at each char and if its not a number
+### or one of my special chars then delete it
+### and keep going...
+    @classmethod
+    def remove_unwanted_chars(self,date):
+        print(date)

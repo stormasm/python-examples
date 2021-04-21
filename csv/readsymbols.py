2b91807c4aaf0ba48b645adad072f20ccc392338
Michael Angerman <stormasm@gmail.com>
2021-04-21 13:04:43 -0700

    update readsymbols.py

--- csv/readsymbols.py
+++ csv/readsymbols.py
@@ -1,8 +1,16 @@
 import csv
-with open('symbols.csv', newline='') as csvfile:
-    symbolreader = csv.reader(csvfile, delimiter=',')
-    symboldict = {};
-    for row in symbolreader:
-        symboldict[row[0]] = row[1]
-    print(symboldict.items())
-    print(symboldict['adbe'])
+import os
+
+path = os.environ['BMTOP']
+symbol_filename = path + '/bluemesa/data/symbols.csv'
+
+def get_symbol_name(symbol):
+    with open(symbol_filename, newline='') as csvfile:
+        symbolreader = csv.reader(csvfile, delimiter=',')
+        symboldict = {};
+        for row in symbolreader:
+            symboldict[row[0]] = row[1]
+        return(symboldict[symbol])
+
+if __name__ == "__main__":
+    print(get_symbol_name('intu'))

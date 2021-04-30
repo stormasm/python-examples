64e1dee50ded4af2f8c65792fff99311ba396fd6
Michael Angerman <stormasm@gmail.com>
2021-04-29 20:08:36 -0700

    part I up and running

--- pandas/dictaryfileread.py
+++ pandas/dictaryfileread.py
@@ -1,117 +1,36 @@
-import csv
+# So this takes in
+# files = os.listdir(path1)
+# and returns a dict with the
+# key being the symbol and the
+# value being the array of filenames
+# for that particular symbol
+#
+# the nice thing about this implementation
+# is that different symbols can have a different
+# number of csv files as input
+
 import os
-import pandas as pd
 from pathlib import PurePath
 import re
-
-def force_float(elt):
-
-    try:
-        return float(elt)
-    except:
-        return elt
-
-def _convert_to_numeric(s):
-
-    if "M" in s:
-        s = s.strip("M")
-        v = force_float(s) / 1000
-        return(round(v,5))
-
-    if "B" in s:
-        s = s.strip("B")
-        return force_float(s)
-
-    return force_float(s)
-
-def modify_value(value):
-    mytype = type(value)
-    bool = isinstance(value,(float,tuple))
-    if not bool:
-        return(_convert_to_numeric(value))
-    else:
-        return("N/A")
-
-def modify_array_values(input):
-    arr = []
-    for value in input:
-        mytype = type(value)
-        bool = isinstance(value,(float,tuple))
-        if not bool:
-            myvalue = _convert_to_numeric(value)
-            arr.append(myvalue)
-        else:
-            arr.append("N/A")
-    return(arr)
 
 def get_symbol_from_filename(filename):
     pp = PurePath(filename)
     # get the filename in the path
     p1 = pp.parts[-1]
     # get everything before the dot
-    p2 = re.split("-fun",p1)[0]
+    p2 = re.split("-",p1)[0]
     return(p2)
 
-def get_series_from_file(filename):
-    df = pd.read_csv(filename, sep=',')
-    #print(df)
-    series = df['Value']
-    values = series.values
-    #print(values)
-    values = modify_array_values(values)
-    #print(values)
-    return(values)
-
-def get_filtered_series_from_file(dicschema,keepary,filename):
-    df = pd.read_csv(filename, sep=',')
-    d = {}
-    for x,y in df.iterrows():
-        origkey = y['Attribute']
-        shortkey = dicschema[origkey]
-        if shortkey in keepary:
-            #print(shortkey,y['Value'])
-            value = y['Value']
-            value = modify_value(value)
-            d[shortkey] = value
-    #print(d)
-    return(pd.Series(d))
-
-# This returns a dict with the original name and the new name
-def read_schema_to_dict(path):
-    d = {}
-    with open(path, newline='') as csvfile:
-        funreader = csv.reader(csvfile, delimiter=',')
-        # do not read the first line of the csv file
-        next(funreader)
-        for row in funreader:
-            d[row[1]] = row[2]
-    return(d)
-
-# This returns a dict with the new name and the original name
-def read_reverse_schema_to_dict(path):
-    d = {}
-    with open(path, newline='') as csvfile:
-        funreader = csv.reader(csvfile, delimiter=',')
-        # do not read the first line of the csv file
-        next(funreader)
-        for row in funreader:
-            d[row[2]] = row[1]
-    return(d)
-
+def add_file_to_dict(dict,symbol,filename):
+    print(symbol)
 
 if __name__ == "__main__":
     pathtop = os.environ['BMTOP']
     path1 = pathtop + '/python-examples/data/csv'
-#   path1 = '/tmp/csv'
-    path2 = pathtop + '/python-examples/data/schema-fun.csv'
-    dic_schema = read_schema_to_dict(path2)
-    ary = ['book-value','free-cash-flow']
     files = os.listdir(path1)
     d = {}
     for file in files:
         filename = os.path.join(path1, file)
-        series = get_filtered_series_from_file(dic_schema,ary,filename)
         symbol = get_symbol_from_filename(filename)
-        d[symbol] = series
-    df = pd.DataFrame(d)
-    print(df)
+        add_file_to_dict(d,symbol,filename)
+    print(d)

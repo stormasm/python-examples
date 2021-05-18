e86453ceecfb8f24bff4f60e3d33ff9a126d654a
Michael Angerman <stormasm@gmail.com>
2021-05-17 18:46:04 -0700

    black format

--- pandas/selectbuild.py
+++ pandas/selectbuild.py
@@ -2,18 +2,21 @@ from dictaryfileread import daryfilereader, get_symbol_from_filename
 import os
 import pandas as pd
 
-def get_df_from_file(filename):
-    df = pd.read_csv(filename, sep=',')
-    return(df)
 
-def concat(symbol,symbol_fileary):
+def get_df_from_file(filename):
+    df = pd.read_csv(filename, sep=",")
+    return df
+
+
+def concat(symbol, symbol_fileary):
     dfary = []
     fileary = symbol_fileary[symbol]
     for file in fileary:
         df = get_df_from_file(file)
         dfary.append(df)
-    result = pd.concat(dfary,ignore_index=True)
-    return(result)
+    result = pd.concat(dfary, ignore_index=True)
+    return result
+
 
 def get_symbols_from_filenames(filenames):
     symbols = set()
@@ -21,22 +24,23 @@ def get_symbols_from_filenames(filenames):
         symbol = get_symbol_from_filename(file)
         symbols.add(symbol)
     # Turn the set into a list
-    return(list(symbols))
+    return list(symbols)
+
 
 if __name__ == "__main__":
-    pathtop = os.environ['BMTOP']
-#   path1 = pathtop + '/python-examples/data/csv'
-    path1 = pathtop + '/bluemesa/tmp/fun/in/test'
+    pathtop = os.environ["BMTOP"]
+    #   path1 = pathtop + '/python-examples/data/csv'
+    path1 = pathtop + "/bluemesa/tmp/fun/in/test"
 
     files = os.listdir(path1)
     symbols = get_symbols_from_filenames(files)
-    d = daryfilereader(path1,files)
+    d = daryfilereader(path1, files)
     for symbol in symbols:
         print(symbol)
-        df = concat(symbol,d)
-        pd.set_option('display.max_rows', None)
-        for index,row in df.iterrows():
-            if row[1] == 'Levered Free Cash Flow (ttm)':
-                print(row[1],row[2])
-            if row[1] == 'Market Cap':
-                print(row[1],row[2])
+        df = concat(symbol, d)
+        pd.set_option("display.max_rows", None)
+        for index, row in df.iterrows():
+            if row[1] == "Levered Free Cash Flow (ttm)":
+                print(row[1], row[2])
+            if row[1] == "Market Cap":
+                print(row[1], row[2])

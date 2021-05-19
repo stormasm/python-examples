a5e174014b4f0005ba10c91b77512b1dbede7da0
Michael Angerman <stormasm@gmail.com>
2021-05-19 10:02:15 -0700

    getfiles in the files directory is successfully using glob to only grab certain files

--- file/getfiles.py
+++ file/getfiles.py
@@ -1,6 +1,15 @@
+import glob
 import os
 from os import listdir
 from os.path import isfile, join
+
+
+def getsymbolfiles(mypath, symbol):
+    hits = []
+    pattern = mypath + "/" + symbol + "-*.csv"
+    for name in glob.glob(pattern):
+        hits.append(name)
+    return hits
 
 
 def getfiles(mypath):
@@ -12,4 +21,7 @@ if __name__ == "__main__":
     path = os.environ["BMTOP"]
     path = path + "/bluemesa/tmp/fun/in/test"
     myfiles = getfiles(path)
+    mysymbolfiles = getsymbolfiles(path, "ibm")
     print(myfiles)
+    print("\n")
+    print(mysymbolfiles)

af80bedfb3e770171f61a505a6715836feb0ed10
Michael Angerman <stormasm@gmail.com>
2021-05-19 10:26:23 -0700

    combined all 3 delfile methods into one big method for ease of use of moving just one method to another place in the file system so this function can be used

--- file/delfiles.py
+++ file/delfiles.py
@@ -6,6 +6,16 @@ from os import listdir
 from os.path import isfile, join
 
 
+def get_and_delete_symbol_files(path, symbol):
+    filenames = []
+    pattern = path + "/" + symbol + "-*.csv"
+    for name in glob.glob(pattern):
+        filenames.append(name)
+    for filename in filenames:
+        if os.path.exists(filename):
+            os.remove(filename)
+
+
 def deletefile(filename):
     if os.path.exists(filename):
         os.remove(filename)
@@ -28,8 +38,13 @@ if __name__ == "__main__":
     path = os.environ["BMTOP"]
     # path = path + "/bluemesa/tmp/fun/in/test"
     path = "/tmp/fun"
-    symbolfiles = getsymbolfiles(path, "ui")
-    print(symbolfiles)
-    for filename in symbolfiles:
-        print(filename)
-        deletefile(filename)
+    #
+    #   This is the first way
+    #
+    #   symbolfiles = getsymbolfiles(path, "ui")
+    #   print(symbolfiles)
+    #   for filename in symbolfiles:
+    #       print(filename)
+    #       deletefile(filename)
+
+    get_and_delete_symbol_files(path, "rdfn")

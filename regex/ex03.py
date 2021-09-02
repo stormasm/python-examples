8d4b7ef0e7aaf43e16d45b542aa489966e3c77fe
Michael Angerman <stormasm@gmail.com>
2021-09-01 22:07:06 -0700

    mw

--- regex/ex03.py
+++ regex/ex03.py
@@ -16,11 +16,16 @@ def get_name_and_number(input):
     #    print('No match')
 
     m = re.match(r"(?P<x1>\w+) (?P<x2>\w+) (?P<x3>has) (?P<x4>\d+)", input)
-    print(m.group('x1'))
-    print(m.group('x2'))
-    print(m.group('x3'))
-    print(m.group('x4'))
-
+    #print(m.group('x1'))
+    #print(m.group('x2'))
+    #print(m.group('x3'))
+    r1 = m.group('x1') + " " + m.group('x2')
+    r2 = m.group('x4')
+    print(r1)
+    print(r2)
+    mytuple = (r1,r2)
+    print(mytuple)
+    return(mytuple)
 
 org01 = "Shewanella colwelliana has 3467 proteins with network connections."
-get_name_and_number(org01)
+print(get_name_and_number(org01))

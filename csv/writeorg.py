d7e350ad6ef2319c2e443021582329f19c9af7d6
Michael Angerman <stormasm@gmail.com>
2021-09-02 10:33:42 -0700

    making progress (finally) with writeorg

--- csv/writeorg.py
+++ csv/writeorg.py
@@ -2,12 +2,28 @@ import csv
 import re
 
 def get_name_and_number(input):
-    print(input)
-    m = re.match(r"(?P<x1>\w+) (?P<x2>\w+) (?P<x3>has) (?P<x4>\d+)", input)
+    m = re.match(r"(?P<x0>Organism) (?P<x1>\w+) (?P<x2>\w+) (?P<x3>has) (?P<x4>\d+)", input)
+    if m == None:
+        return None
     r1 = m.group("x1") + " " + m.group("x2")
     r2 = m.group("x4")
     mytuple = (r1, r2)
     return mytuple
+
+
+
+#def get_name_and_number(input):
+#    print(type(input))
+#    print(input)
+#    m = re.match(r"(?P<x1>\w+) (?P<x2>\w+) (?P<x3>has) (?P<x4>\d+)", input)
+#    print(m)
+#    if m == None:
+#        return None
+#    r1 = m.group("x1") + " " + m.group("x2")
+#    r2 = m.group("x4")
+#    mytuple = (r1, r2)
+#    print(mytuple)
+#    return mytuple
 
 def process_row(row):
     value = row[1]
@@ -15,7 +31,8 @@ def process_row(row):
     org01 = "Shewanella colwelliana has 3467 proteins with network connections."
     #print(type(value))
     result = get_name_and_number(value)
-    #print(result)
+    if result != None:
+        print(result)
 
 def read_org():
     with open('organism-orig.csv', newline='') as csvfile:

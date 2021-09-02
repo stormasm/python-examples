d5843e0a1072359031b6b516fbba6641e40f5b52
Michael Angerman <stormasm@gmail.com>
2021-09-01 22:09:23 -0700

    code format

--- regex/ex00.py
+++ regex/ex00.py
@@ -1,19 +1,19 @@
-
 ## Reference
 ## https://stackoverflow.com/questions/17710921/python-removing-last-character-from-string-using-regex
 ##
 
 import re
-a = ["SDFSD_SFSDF234234","SDFSDF_SDFSDF_234324","TSFSD_SDF_213123"]
-c = [re.sub('_?\d+','',x) for x in a]
+
+a = ["SDFSD_SFSDF234234", "SDFSDF_SDFSDF_234324", "TSFSD_SDF_213123"]
+c = [re.sub("_?\d+", "", x) for x in a]
 print(c)
 
 # Replace every white-space character with the number 9:
 txt = "The rain in Spain"
-x = re.sub('\s', '9', txt)
+x = re.sub("\s", "9", txt)
 print(x)
 
 # Replace every M,B,% character with nothing:
-a = ["123.45B","23B","234M","434.12B","34%","56.1%"]
-c = [re.sub('[M,B,%]', '', x) for x in a]
+a = ["123.45B", "23B", "234M", "434.12B", "34%", "56.1%"]
+c = [re.sub("[M,B,%]", "", x) for x in a]
 print(c)
--- regex/ex02.py
+++ regex/ex02.py
@@ -1,13 +1,15 @@
 import re
 
+
 def get_date_from_filename(input):
-    p = re.compile('(-)[0-9]*(_)')
+    p = re.compile("(-)[0-9]*(_)")
     q = p.search(input)
     x = q.group()
     x = x[1:9]
-    return(x)
+    return x
 
-a = ["sq-20191231_htm.xml","cube-20200331_htm.xml"]
+
+a = ["sq-20191231_htm.xml", "cube-20200331_htm.xml"]
 for x in a:
     value = get_date_from_filename(x)
     print(value)

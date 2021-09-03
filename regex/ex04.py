686f345dd1f96c56db1fc541ed56a57a953a460d
Michael Angerman <stormasm@gmail.com>
2021-09-02 20:14:51 -0700

    mw

--- regex/ex04.py
+++ regex/ex04.py
@@ -2,12 +2,16 @@
 
 import re
 
+
 def get_name_and_number(input):
-    m = re.match(r"(?P<x0>Organism) (?P<x1>\w+) (?P<x2>\w+) (?P<x3>has) (?P<x4>\d+)", input)
+    m = re.match(
+        r"(?P<x0>Organism) (?P<x1>\w+) (?P<x2>\w+) (?P<x3>has) (?P<x4>\d+)", input
+    )
     r1 = m.group("x1") + " " + m.group("x2")
     r2 = m.group("x4")
     mytuple = (r1, r2)
     return mytuple
+
 
 org01 = "Organism Shewanella colwelliana has 3467 proteins with network connections."
 print(get_name_and_number(org01))
